408f05d97d623c91ad9a5ae3926a7b4ce3e93523
koustav-gh <koustavg67@gmail.com>
2025-04-21 19:48:56 +0530

    major changes

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
 
   const handleSend = async () => {
     if (message.trim()) {
-      setError('') // Clear error before sending a new request
+      setError('')
       setMessages((prev) => [...prev, { sender: 'user', content: message }])
       const currentMessage = message
       setMessage('')
